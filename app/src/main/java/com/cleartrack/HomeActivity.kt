ce8471cb7f737797ed1678d63887101fba9c6993
Rishabh Raj <rishabhraj1572@gmail.com>
2024-10-23 21:17:19 +0530

    update

--- app/src/main/java/com/cleartrack/HomeActivity.kt
+++ app/src/main/java/com/cleartrack/HomeActivity.kt
@@ -1,14 +1,27 @@
 package com.cleartrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        auth = FirebaseAuth.getInstance()
 
+        var logout: Button = findViewById(R.id.logout)
+
+        logout.setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+            finishAffinity()
+        }
     }
 }
