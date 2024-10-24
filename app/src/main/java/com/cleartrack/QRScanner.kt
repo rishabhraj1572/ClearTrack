591f006e1322a4487524edfd422c790af7175e45
Rishabh Raj <rishabhraj1572@gmail.com>
2024-10-24 19:24:48 +0530

    update

--- app/src/main/java/com/cleartrack/QRScanner.kt
+++ app/src/main/java/com/cleartrack/QRScanner.kt
@@ -1,14 +1,18 @@
 package com.cleartrack
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.github.g00fy2.quickie.QRResult
 import io.github.g00fy2.quickie.ScanQRCode
 
 
 class QRScanner : AppCompatActivity() {
-    val scanQrCodeLauncher = registerForActivityResult(ScanQRCode()) { result ->
-        // Handle the QR result
 
+    private var qrContent: String? = null
+    private val scanQrCodeLauncher = registerForActivityResult(ScanQRCode()) { result ->
+        getResult(result)
     }
 
 
@@ -16,5 +20,24 @@ class QRScanner : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scanner)
         scanQrCodeLauncher.launch(null)
+//        finish()
+
+
+    }
+
+
+    private fun getResult(result: QRResult) {
+        qrContent = when (result) {
+            is QRResult.QRSuccess -> {
+                result.content.rawValue
+                    ?: result.content.rawBytes?.let { String(it) }.orEmpty()
+            }
+            QRResult.QRUserCanceled -> "User canceled"
+            QRResult.QRMissingPermission -> "Missing permission"
+            is QRResult.QRError -> "${result.exception.javaClass.simpleName}: ${result.exception.localizedMessage}"
+        }
+
+        Log.d("QR RESULT", "QR content: $qrContent")
+//        Toast.makeText(this, qrContent ?: "No content found", Toast.LENGTH_SHORT).show()
     }
 }
