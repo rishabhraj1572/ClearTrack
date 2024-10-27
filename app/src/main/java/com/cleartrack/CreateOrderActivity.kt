000cde7d4c7f77ae8e5026c4794e836f747f580e
Rishabh Raj <rishabhraj1572@gmail.com>
2024-10-27 13:40:27 +0530

    uploading files done

--- app/src/main/java/com/cleartrack/CreateOrderActivity.kt
+++ app/src/main/java/com/cleartrack/CreateOrderActivity.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class CreateOrderActivity : AppCompatActivity() {
 
@@ -22,25 +24,38 @@ class CreateOrderActivity : AppCompatActivity() {
     private lateinit var expAttStatus: TextView
     private lateinit var impAttStatus: TextView
     private lateinit var goodsAttStatus: TextView
+    private lateinit var submitBtn: Button
 
     private var currentStatusTextView: TextView? = null
 
     companion object {
         private const val PICK_IMAGE_REQUEST = 1
-        private const val PICK_PDF_REQUEST = 2
+        private const val PICK_PDF_REQUEST_EXPORTER = 2
+        private const val PICK_PDF_REQUEST_IMPORTER = 3
+        private const val PICK_PDF_REQUEST_GOODS = 4
     }
+
+    private var imgUri: Uri? = null
+    private var orderID: String? = null
+    
+    private var expDocUri: Uri?=null
+    private var impDocUri: Uri?=null
+    private var goodsDocUri: Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
         auth = FirebaseAuth.getInstance()
+        orderID = auth.currentUser?.let { orderId(it.uid) }
+
         imageView = findViewById(R.id.select_photo)
 
         // Initialize buttons and status TextViews
         expDocButton = findViewById(R.id.exp_doc)
         impDocButton = findViewById(R.id.imp_doc)
         goodsDocButton = findViewById(R.id.goods_doc)
+        submitBtn = findViewById(R.id.submiBtn)
 
         expAttStatus = findViewById(R.id.exp_att_status)
         impAttStatus = findViewById(R.id.imp_att_status)
@@ -54,17 +69,67 @@ class CreateOrderActivity : AppCompatActivity() {
         // click listeners for document upload buttons
         expDocButton.setOnClickListener {
             currentStatusTextView = expAttStatus // Set the current status TextView
-            uploadPdf("Exporter Document")
+            attachPDF("Exporter Document")
         }
 
         impDocButton.setOnClickListener {
             currentStatusTextView = impAttStatus // Set the current status TextView
-            uploadPdf("Importer Document")
+            attachPDF("Importer Document")
         }
 
         goodsDocButton.setOnClickListener {
             currentStatusTextView = goodsAttStatus // Set the current status TextView
-            uploadPdf("Goods Document")
+            attachPDF("Goods Document")
+        }
+
+
+        //submitBtn
+        submitBtn.setOnClickListener{
+
+            //uploading all attached files
+            if(imgUri == null || expDocUri==null||impDocUri==null||goodsDocUri==null){
+                Toast.makeText(this,"Please attach required files",Toast.LENGTH_SHORT).show()
+            }else{
+                uploadDocsToFirebase(orderID,"user_image",imgUri,false)
+                uploadDocsToFirebase(orderID,"exporter_doc",expDocUri,true)
+                uploadDocsToFirebase(orderID,"importer_doc",impDocUri,true)
+                uploadDocsToFirebase(orderID,"goods_doc",goodsDocUri,true)
+            }
+
+
+            //firestore work here
+
+
+
+        }
+    }
+
+    //defining order id
+    private fun orderId(userId: String): String {
+        val currentTime = System.currentTimeMillis()
+        return userId+"_"+currentTime
+    }
+
+    //uploading files
+    private fun uploadDocsToFirebase(orderID : String?,doc_name :String, uri : Uri?, is_pdf : Boolean){
+
+        var fileReference: StorageReference?
+        
+        if(is_pdf){
+            fileReference = FirebaseStorage.getInstance().reference.child("order_docs/$orderID/$doc_name.pdf")
+        }else{
+            fileReference = FirebaseStorage.getInstance().reference.child("order_docs/$orderID/$doc_name.jpg")
+        }
+
+
+        if (uri != null) {
+            fileReference.putFile(uri)
+                .addOnSuccessListener {
+    //                Toast.makeText(this, "File uploaded successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Failed to upload file $doc_name", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
@@ -73,13 +138,21 @@ class CreateOrderActivity : AppCompatActivity() {
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
 
-    private fun uploadPdf(docType: String) {
+    private fun attachPDF(docType: String) {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
             type = "application/pdf"
             addCategory(Intent.CATEGORY_OPENABLE)
         }
+        
+        if(docType == "Exporter Document"){
+            startActivityForResult(Intent.createChooser(intent, "Select PDF"), PICK_PDF_REQUEST_EXPORTER)
+        }else if(docType == "Importer Document"){
+            startActivityForResult(Intent.createChooser(intent, "Select PDF"), PICK_PDF_REQUEST_IMPORTER)
+        }else if(docType == "Goods Document"){
+            startActivityForResult(Intent.createChooser(intent, "Select PDF"), PICK_PDF_REQUEST_GOODS)
+        }
 
-        startActivityForResult(Intent.createChooser(intent, "Select PDF"), PICK_PDF_REQUEST)
+       
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -89,23 +162,53 @@ class CreateOrderActivity : AppCompatActivity() {
             PICK_IMAGE_REQUEST -> {
                 if (resultCode == RESULT_OK && data != null) {
                     val imageUri: Uri? = data.data
-                    imageView.setImageURI(imageUri) // Display the selected image in the ImageView
-                    // Handle uploading or other purposes for the image
+                    imageView.setImageURI(imageUri) //replace with glide or set image size up to 500kb
+
+                    //imageUri here
+                    imgUri = imageUri
+
                 }
             }
-            PICK_PDF_REQUEST -> {
+            PICK_PDF_REQUEST_EXPORTER -> {
                 if (resultCode == RESULT_OK && data != null) {
                     val pdfUri: Uri? = data.data
                     if (pdfUri != null) {
-                        // Handle the PDF upload logic here
-
-                        // Update the attached status
                         currentStatusTextView?.text = "Attached"
 
-                        // Optionally, you could show a message that a new document is attached
+                        //pdf uri here
+                        expDocUri = pdfUri
                         Toast.makeText(this, "Document attached successfully", Toast.LENGTH_SHORT).show()
                     } else {
-                        // Handle error if the URI is null
+                        Toast.makeText(this, "Failed to select PDF", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            PICK_PDF_REQUEST_IMPORTER -> {
+                if (resultCode == RESULT_OK && data != null) {
+                    val pdfUri: Uri? = data.data
+                    if (pdfUri != null) {
+                        currentStatusTextView?.text = "Attached"
+
+                        //pdf uri here
+                        impDocUri = pdfUri
+                        Toast.makeText(this, "Document attached successfully", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Failed to select PDF", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            
+            PICK_PDF_REQUEST_GOODS -> {
+                if (resultCode == RESULT_OK && data != null) {
+                    val pdfUri: Uri? = data.data
+                    if (pdfUri != null) {
+                        currentStatusTextView?.text = "Attached"
+
+                        //pdf uri here
+                        goodsDocUri = pdfUri
+                        Toast.makeText(this, "Document attached successfully", Toast.LENGTH_SHORT).show()
+                    } else {
                         Toast.makeText(this, "Failed to select PDF", Toast.LENGTH_SHORT).show()
                     }
                 }
